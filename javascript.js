3529292b778ff41e83083d3052860a46afa33264
Rachit Agarwal <151500126+rachitagarwal1990@users.noreply.github.com>
2026-08-26 09:41:52 +0530

    Put calculator layout
    added buttons for numbers, operators and clear. added eventlistener
    to store the first number

---
+++ javascript.js
@@ -0,0 +1,87 @@
+let add = function(a,b) {
+    return a+b ;
+}
+
+let subtract = function(a,b) {
+    return a-b ;
+}
+
+let multiply = function(a,b) {
+    return a*b ;
+}
+
+let divide = function(a,b) {
+    if (b==0){
+        alert("Cannot divide by 0")
+    }
+    else{
+        return a/b;
+    }
+}
+
+let operate = function(firstNumber, operator, secondNumber){
+    switch(operator){
+        case "+":
+            return add(firstNumber, secondNumber);
+            break;
+        case "-":
+            return subtract(firstNumber, secondNumber);
+            break;
+        case "x":
+            return multiply(firstNumber, secondNumber);
+            break;
+        case "/":
+            return divide(firstNumber, secondNumber);
+            break;            
+    }
+}
+
+let createCalculatorButton = function(buttonType, buttonText){
+    let newButton = document.createElement("button");
+    newButton.textContent = `${buttonText}`;
+    newButton.classList.add(`${buttonText}`);
+    newButton.style.width = `${buttonSize}px`;
+    newButton.style.height = `${buttonSize/2}px`;
+    switch(buttonType){
+        case "numbers":
+            numbers.appendChild(newButton);
+            newButton.addEventListener("click", (e) => {
+                firstNumber = e.target.textContent;
+                display.textContent = e.target.textContent;
+            })
+            break;
+        case "operators":
+            operators.appendChild(newButton);
+            break;
+        case "utilities":
+            utilities.appendChild(newButton);
+            break;
+    }
+}
+
+const container = document.querySelector(".container");
+const numbers   = document.querySelector(".numbers");
+const operators = document.querySelector(".operators");
+const utilities = document.querySelector(".utilities");
+const display   = document.querySelector(".display");
+
+const buttonSize = container.offsetWidth / 4;
+
+let allButtons = [
+    {buttonType: "numbers",     buttonValues: [1,2,3,4,5,6,7,8,9,0] },
+    {buttonType: "operators",   buttonValues: ["+","-","x","/"]     },
+    {buttonType: "utilities",   buttonValues: ["=","Clear"]         }
+]
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+allButtons.map(function(buttonGroup) {
+    buttonGroup.buttonValues.map( button => 
+        createCalculatorButton(buttonGroup.buttonType, button )
+    )
+});
+
+
+let a =1;
